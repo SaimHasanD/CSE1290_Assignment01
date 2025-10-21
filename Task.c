3e2bbad07d4c2c24426db3e0fe00f0ef3e5fbdb6
SaimHasanD <saimhasan514@gmail.com>
2025-10-21 19:29:54 +0600

    Task 2

--- Task.c
+++ Task.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 
 int main() {
+
+
+    //Task 1
+    /*
     int i;
+
     printf("Odd numbers from 200 to 250:\n");
+
     for (i = 200; i <= 250; i++) {
         if (i % 2 != 0)
             printf("%d ", i);
+    }*/
+
+    //Task 2
+    int numbers[5], i, largest;
+
+    printf("Enter 5 integers:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &numbers[i]);
     }
+
+    largest = numbers[0];
+    for (i = 1; i < 5; i++) {
+        if (numbers[i] > largest)
+            largest = numbers[i];
+    }
+
+    printf("The largest number is: %d\n", largest);
+
+
     return 0;
 }
